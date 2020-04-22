89a71ce39630e91b87639d668e6f3abcfbec0375
willnaoosmit <55159911+willnaoosmit@users.noreply.github.com>
2020-04-22 10:08:14 -0300

    Update InnerButton.js

--- dist/common/InnerButton.js
+++ dist/common/InnerButton.js
@@ -1,7 +1,6 @@
 Object.defineProperty(exports,"__esModule",{value:true});var _jsxFileName='src/common/InnerButton.js';var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value"in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require('react');var _react2=_interopRequireDefault(_react);
 var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);
 var _reactNative=require('react-native');
-var _reactNativeCircularProgress=require('react-native-circular-progress');
 var _reactNativeSpinkit=require('react-native-spinkit');var _reactNativeSpinkit2=_interopRequireDefault(_reactNativeSpinkit);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}
 
 var styles=_reactNative.StyleSheet.create({
@@ -145,16 +144,10 @@ this.props.text)];
 var progressAnim=this.props.textInsideProgress?
 this.props.progressAnim:
 this.props.imageAnim;
+
 progress=
 _react2.default.createElement(_reactNative.Animated.View,{style:[styles.progress,this.props.progressStyle,progressAnim],__source:{fileName:_jsxFileName,lineNumber:149}},
-_react2.default.createElement(_reactNativeCircularProgress.AnimatedCircularProgress,{
-size:this.props.progressSize,
-width:this.props.progressWidth,
-fill:this.props.progressFill,
-tintColor:this.props.progressTintColor,
-backgroundColor:this.props.progressBackgroundColor,__source:{fileName:_jsxFileName,lineNumber:150}},
-
-progressContent));
+);
 
 
 
